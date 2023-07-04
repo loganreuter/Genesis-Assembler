2b9a0080802363a4e08ef6338458c5ee68670980
lreuter2020 <lreuter2020@gmail.com>
2023-07-03 22:20:53 -0700

    finished regex functions

--- asm/regex.c
+++ asm/regex.c
@@ -18,5 +18,21 @@ int match(char *regex, char *text)
 
 int matchhere(char *regex, char *text)
 {
-    
+    if(regex[0] == '\0')
+        return 1;
+    if(regex[1] == '*')
+        return matchstar(regex[0], regex+2, text);
+    if(regex[0] == '$' && regex[1] == '\0')
+        return *text == '\0';
+    if(*text != '\0' && (regex[0] == '.' || regex[0] == *text))
+        return matchhere(regex + 1, text + 1);
+}
+
+int matchstart(int c, char *regex, char *text)
+{
+    do{
+        if(matchhere(regex, text))
+            return 1;
+    } while (*text != '\0' && (*text++ == c || c == '.'));
+    return 0;
 }
